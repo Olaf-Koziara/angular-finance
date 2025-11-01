4c37f14ca717d90543194f73756d90852c24d92e
Olaf1303 <olaf.koziara@gmail.com>
2025-11-01 09:57:40 +0100

    Add TrendsSection component with HTML, SCSS, and TypeScript files

---
+++ client/src/app/features/dashboard/components/trends-section/trends-section.component.ts
@@ -0,0 +1,21 @@
+import { Component, input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { TranslateModule } from '@ngx-translate/core';
+import { TrendData } from '../../models/dashboard.models';
+import { formatCurrency, formatPercentage } from '../../utils/format.utils';
+
+@Component({
+  selector: 'app-trends-section',
+  standalone: true,
+  imports: [MatCardModule, MatIconModule, TranslateModule],
+  templateUrl: './trends-section.component.html',
+  styleUrl: './trends-section.component.scss'
+})
+export class TrendsSectionComponent {
+  trends = input.required<TrendData[]>();
+  
+  formatCurrency = formatCurrency;
+  formatPercentage = formatPercentage;
+}
+
