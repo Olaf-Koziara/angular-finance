c2496f699f63ecc2edabb23d87f2988172444056
Olaf1303 <olaf.koziara@gmail.com>
2025-11-06 00:26:57 +0100

    Update app routes to change default path to 'transactions': Adjusted routing configuration to load the TransactionsPageComponent when navigating to the new path.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,10 +2,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'transactions',
     loadComponent: () =>
       import('./features/transactions/pages/transactions-page.component').then(
-        m => m.TransactionsPageComponent,
+        (m) => m.TransactionsPageComponent
       ),
   },
   {
