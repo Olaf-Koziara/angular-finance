080d7bd49e6cd071da8aa888648ea12b7999b3c6
Olaf1303 <olaf.koziara@gmail.com>
2025-11-06 00:27:39 +0100

    Update app component to include RouterOutlet: Added RouterOutlet to the app component imports for improved routing functionality.

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, OnInit, signal } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { IconsService } from './core/icons/icons.service';
 import { SidebarComponent } from './core/layout/sidebar/sidebar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
@@ -13,7 +14,7 @@ export class App implements OnInit {
   protected readonly title = signal('angular-finance');
 
   constructor(private translate: TranslateService, private iconsService: IconsService) {
-    translate.use('en');
+    this.translate.use('en');
   }
 
   ngOnInit(): void {
