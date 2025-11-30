63327c0d04f7a021f7b6091fa761167c84f621e9
Olaf1303 <olaf.koziara@gmail.com>
2025-11-30 17:27:59 +0100

    move transactions route to protected routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -23,6 +23,16 @@ export const routes: Routes = [
             (m) => m.DashboardComponent
           ),
       },
+      {
+        path: 'transactions',
+        loadComponent: () =>
+          import('./features/transactions/pages/transactions-page.component').then(
+            (m) => m.TransactionsPageComponent
+          ),
+        data: {
+          pageTranslationName: 'TRANSACTIONS',
+        },
+      },
     ],
   },
   {
@@ -40,16 +50,7 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./features/unauthorized/unauthorized.component').then((m) => m.UnauthorizedComponent),
   },
-  {
-    path: 'transactions',
-    loadComponent: () =>
-      import('./features/transactions/pages/transactions-page.component').then(
-        (m) => m.TransactionsPageComponent
-      ),
-    data: {
-      pageTranslationName: 'TRANSACTIONS',
-    },
-  },
+
   {
     path: '**',
     redirectTo: '',
