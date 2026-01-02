6308a201c8370fa3353ef280d9fc3400b1357df2
Olaf1303 <olaf.koziara@gmail.com>
2026-01-02 17:24:16 +0100

    refactor(client): consolidate settings page into settings dialog with enhanced appearance and preferences options

--- client/src/app/features/settings/components/settings-dialog/settings-dialog.component.ts
+++ client/src/app/features/settings/components/settings-dialog/settings-dialog.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject } from '@angular/core';
-import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
-import { LucideAngularModule, Palette, Wallet, Settings as SettingsIcon } from 'lucide-angular';
-import { SettingsService } from '../../services/settings.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { LucideAngularModule, Palette, Settings as SettingsIcon, Wallet } from 'lucide-angular';
 import { Settings } from '../../models/settings.model';
-import { SettingsPage } from "../../pages/settings-page/settings-page";
+import { SettingsService } from '../../services/settings.service';
+import { BackgroundPicker } from '../background-picker/background-picker.component';
+import { CurrencyPicker } from '../currency-picker/currency-picker';
 
 @Component({
     selector: 'app-settings-dialog',
@@ -13,7 +15,9 @@ import { SettingsPage } from "../../pages/settings-page/settings-page";
         MatDialogModule,
         MatButtonModule,
         LucideAngularModule,
-        SettingsPage
+        MatCardModule,
+        BackgroundPicker, 
+        CurrencyPicker, 
     ],
     templateUrl: './settings-dialog.component.html',
     styleUrl: './settings-dialog.component.scss'
@@ -21,5 +25,10 @@ import { SettingsPage } from "../../pages/settings-page/settings-page";
 export class SettingsDialogComponent {
     readonly settingsService = inject(SettingsService);
     readonly icons = { Palette, Wallet, Settings: SettingsIcon };
+
+  
+    updateSetting(setting: keyof Settings, value: string | undefined) {
+      this.settingsService.updateSetting(setting, value || '');
+    }
 
 }
