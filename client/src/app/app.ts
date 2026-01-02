b79d396091f614a547074f735fbd7bc6166a5f68
Olaf1303 <olaf.koziara@gmail.com>
2026-01-02 17:25:14 +0100

    refactor(client): enhance app component to include backgrounds constant and clean up imports

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,20 +1,21 @@
+import { NgStyle } from '@angular/common';
 import { Component, inject, OnInit, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { IconsService } from './core/icons/icons.service';
-import { SidebarComponent } from './core/layout/sidebar/sidebar.component';
-import { RouterOutlet } from '@angular/router';
 import { SettingsService } from './features/settings/services/settings.service';
-import { NgStyle } from '@angular/common';
+import { backgrounds } from './shared/constants/backgrounds.constants';
 
 @Component({
   selector: 'app-root',
-  imports: [SidebarComponent, RouterOutlet, NgStyle],
+  imports: [ RouterOutlet, NgStyle],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App implements OnInit {
   protected readonly title = signal('angular-finance');
   readonly settingsService = inject(SettingsService);
+  readonly backgrounds = backgrounds;
   selectedBackground = this.settingsService.background;
   constructor(private translate: TranslateService, private iconsService: IconsService) {
     this.translate.use('en');
