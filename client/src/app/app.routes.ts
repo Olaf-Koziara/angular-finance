6e5caca31889cf7a861dec32a8a3ff2d161ba1c3
Olaf1303 <olaf.koziara@gmail.com>
2026-01-02 17:24:55 +0100

    refactor(client): remove settings route from app.routes.ts

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -46,16 +46,7 @@ export const routes: Routes = [
           pageTranslationName: 'TRANSACTIONS',
         },
       },
-      {
-        path: 'settings',
-        loadComponent: () =>
-          import('./features/settings/pages/settings-page/settings-page').then(
-            (m) => m.SettingsPage
-          ),
-        data: {
-          pageTranslationName: 'SETTINGS',
-        },
-      },
+  
     ],
   },
 
