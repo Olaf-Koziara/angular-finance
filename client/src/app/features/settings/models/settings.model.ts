8087ce585ef1c7852751175810fe1a5027e0c842
Olaf1303 <olaf.koziara@gmail.com>
2026-01-02 12:39:47 +0100

    refactor(client): update settings model and service to support currency

--- client/src/app/features/settings/models/settings.model.ts
+++ client/src/app/features/settings/models/settings.model.ts
@@ -1,4 +1,11 @@
 export interface Settings {
   background?: string;
   theme?: string;
+  currency?: string;
+}
+export interface SettingsResponse {
+  success: boolean;
+  data: Settings;
+  message: string;
+
 }
--- client/src/app/features/settings/services/settings.service.ts
+++ client/src/app/features/settings/services/settings.service.ts
@@ -1,13 +1,27 @@
-import { computed, Injectable, signal } from '@angular/core';
-import { Settings } from '../models/settings.model';
+import { computed, inject, Injectable, signal } from '@angular/core';
+import { Settings, SettingsResponse } from '../models/settings.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
+
 export class SettingsService {
   readonly settings = signal<Settings>({});
   readonly background = computed(() => this.settings().background);
-  updateSetting(setting: keyof Settings, value: string) {
-    this.settings.update((prevValue) => ({ ...prevValue, [setting]: value }));
+  readonly theme = computed(() => this.settings().theme);
+  readonly currency = computed(() => this.settings().currency);
+  private readonly http = inject(HttpClient);
+  constructor() {
+    this.fetchSettings();
+  }
+  updateSetting<K extends keyof Settings>(setting: K, value: Settings[K]) {
+    this.settings.update((s) => ({ ...s, [setting]: value }));
+    this.http.patch(`/settings/${setting}`, { value }).subscribe();
+  }
+  private fetchSettings() {
+    this.http.get<SettingsResponse>('/settings').subscribe((settings) => {
+      this.settings.set(settings.data);
+    });
   }
 }
