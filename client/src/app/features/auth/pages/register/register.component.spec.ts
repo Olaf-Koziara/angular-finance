9c77dd5ac56a20dbc44cb8e74958ca0fe2cc9179
Olaf1303 <olaf.koziara@gmail.com>
2026-01-07 20:34:37 +0100

    test(client): enhance register component tests by adding UrlTree mock for router navigation

--- client/src/app/features/auth/pages/register/register.component.spec.ts
+++ client/src/app/features/auth/pages/register/register.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, UrlTree } from '@angular/router';
 import { of, throwError } from 'rxjs';
 import { AuthService } from '../../services/auth.service';
 import { RegisterComponent } from './register.component';
@@ -22,7 +22,12 @@ describe('RegisterComponent', () => {
   beforeEach(async () => {
     authService = jasmine.createSpyObj<AuthService>('AuthService', ['register']);
     router = jasmine.createSpyObj<Router>('Router', ['navigate', 'createUrlTree']);
-    (router.createUrlTree as jasmine.Spy).and.returnValue({});
+    const mockUrlTree: UrlTree = {
+      root: {} as any,
+      queryParams: {},
+      fragment: null,
+    } as UrlTree;
+    (router.createUrlTree as jasmine.Spy).and.returnValue(mockUrlTree);
     
     const activatedRouteMock = {
       root: {
